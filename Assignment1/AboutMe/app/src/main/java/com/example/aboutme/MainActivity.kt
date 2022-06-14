279a26e58a722c69d73b3d7b9f14784459478a43
JoyKarmoker <Joykarmoker353@gmail.com>
2022-06-14 16:29:57 +0600

    Implementing Data Binding

--- Assignment1/AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Assignment1/AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,21 +7,27 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val myName : MyName = MyName("Joy Karmoker")
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val doneButton = findViewById<Button>(R.id.done_button)
-        val nickNameText = findViewById<TextView>(R.id.nickname_text)
-        val nicknameEdit = findViewById<TextView>(R.id.nickname_edit)
-
-        doneButton.setOnClickListener{v ->
-            nickNameText.text = nicknameEdit.text
-            nicknameEdit.visibility = View.GONE
-            doneButton.visibility = View.GONE
-            nickNameText.visibility = View.VISIBLE
+        binding.myName = myName
+        binding.doneButton.setOnClickListener{v ->
+            binding.apply {
+                //nicknameText.text = nicknameEdit.text
+                myName?.nickname = nicknameEdit.text.toString()
+                invalidateAll()
+                nicknameEdit.visibility = View.GONE
+                doneButton.visibility = View.GONE
+                nicknameText.visibility = View.VISIBLE
+            }
 
             //Hide The Keyboard
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
