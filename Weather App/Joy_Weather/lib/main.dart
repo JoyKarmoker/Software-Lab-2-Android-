68fca21813e893955ae9beb4eb895c08d6204ca1
JoyKarmoker <Joykarmoker353@gmail.com>
2022-09-27 18:50:51 +0600

    Trying to parse JSON

---
+++ Weather App/Joy_Weather/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:joy_weather/Activity/home.dart';
+import 'package:joy_weather/Activity/location.dart';
+import 'package:joy_weather/Activity/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Home(),
+  ));
+}
